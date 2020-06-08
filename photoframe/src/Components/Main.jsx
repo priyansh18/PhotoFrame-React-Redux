19e0fbbd13e03e27272e8f5a086056a41edab5e7
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-08 16:51:10 +0530

    Title Rendering and Styling

---
+++ photoframe/src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './Title'
+
+class Main extends Component {
+  render() { 
+    return (
+      <div>
+        <Title title={'PhotoFrame'} />
+      </div>
+      );
+  }
+}
+ 
+export default Main;
