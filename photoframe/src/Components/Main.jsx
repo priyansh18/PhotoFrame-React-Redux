b608428334808f2e70d74e5070f6ac37178b9941
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-09 11:48:26 +0530

    State management without constructor

--- photoframe/src/Components/Main.jsx
+++ photoframe/src/Components/Main.jsx
@@ -1,36 +1,39 @@
 import React, { Component } from "react";
 import Title from "./Title";
-import PhotoFrame from './PhotoFrame';
+import PhotoFrame from "./PhotoFrame";
 
-const allPhotos = [
-  {
-    id: "0",
-    description: "beautiful sea",
-    imageLink: "https://picsum.photos/800/600?image=14",
-  },
-  {
-    id: "1",
-    description: "Coffee cup",
-    imageLink: "https://picsum.photos/800/600?image=30",
-  },
-  {
-    id: "2",
-    description: "Freedom",
-    imageLink: "https://picsum.photos/800/600?image=50",
-  },
-  {
-    id: "3",
-    description: "Farm",
-    imageLink: "https://picsum.photos/800/600?image=85",
-  },
-];
+const allPhotos = [];
 
 class Main extends Component {
+  state = {
+    allPhotos: [
+      {
+        id: "0",
+        description: "beautiful sea",
+        imageLink: "https://picsum.photos/800/600?image=14",
+      },
+      {
+        id: "1",
+        description: "Coffee cup",
+        imageLink: "https://picsum.photos/800/600?image=30",
+      },
+      {
+        id: "2",
+        description: "Freedom",
+        imageLink: "https://picsum.photos/800/600?image=50",
+      },
+      {
+        id: "3",
+        description: "Farm",
+        imageLink: "https://picsum.photos/800/600?image=85",
+      },
+    ],
+  };
   render() {
     return (
       <div>
         <Title title={"PhotoFrame"} />
-        <PhotoFrame photoBlock={allPhotos} />
+        <PhotoFrame photoBlock={this.state.allPhotos} />
       </div>
     );
   }
