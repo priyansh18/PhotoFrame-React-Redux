307465ab74f285cfd8534fbdd09eb73c6dec5c43
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-09 13:56:17 +0530

    UI setup for AddPhoto page

--- photoframe/src/Components/AddPhoto.jsx
+++ photoframe/src/Components/AddPhoto.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddPhoto extends Component {
-  render() { 
-    return ( <h1>This is page fro rendering images</h1> );
+  render() {
+    return (
+      <div>
+        <h1>Photo Frame</h1>
+        <div className="form">
+          <form>
+            <input type="text" placeholder="Link"/>
+            <input type="text" placeholder="Description"/>
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
- 
+
 export default AddPhoto;
