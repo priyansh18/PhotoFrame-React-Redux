c5d90d207915f02533650ca2a4dad04ed86fac0a
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-10 11:36:45 +0530

    Basic Redux installation

--- photoframe/src/index.js
+++ photoframe/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Main from "./Components/Main";
 import "./styles/stylesheet.css";
 import { BrowserRouter } from "react-router-dom";
+import {createStore} from 'redux'
 
 ReactDOM.render(
   <BrowserRouter>
